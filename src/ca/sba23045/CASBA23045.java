cef53815aeb41d2f2c376ed966eb348c0a6453cb
sba23045 <152573259+sba23045@users.noreply.github.com>
2024-01-25 03:03:42 +0000

    Create the reader and Add the File

--- src/ca/sba23045/CASBA23045.java
+++ src/ca/sba23045/CASBA23045.java
@@ -18,9 +18,36 @@ public class CASBA23045 {
     public static void main(String[] args) {
     try (Scanner scanner = new Scanner(System.in)) {
         System.out.println("1. Standard Operation");
+        System.out.println("2. Add Data to Status.txt");
+        System.out.print("Choose an Option: ");
+        int choice = scanner.nextInt();
         
-        
+        switch (choice) {
+            case 1 : verifiedStudentFromFile();
+            case 2 : addDataToStatusFile();
+            default : System.out.println ("Invalid choice. Exist choices 1 or 2");
+        }
+    }
+                
+            
+    }
+
+    private static void verifiedStudentFromFile() {
+        try (BufferedReader br = new BufferedReader (new FileReader("students.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String firstName = line.trim();
+                String secondName = line.trim();
+                int numClasses = Integer.parseInt(br.readLine().trim());
+                String studentID = br.readLine().trim();
+                
+            }
+        }
+        }
+
+    private static void addDataToStatusFile() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     }
-    
-}
+ 
+
